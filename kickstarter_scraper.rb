967afc9bc85c1f0f184a17a9ba3296bc6c4d7675
Zach  Smith <zdsmith001@gmail.com>
2019-09-06 19:42:44 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -22,10 +22,12 @@ def create_project_hash
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
+
     projects
   end
+
 end
 
 
